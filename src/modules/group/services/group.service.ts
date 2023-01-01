76611e4ec6979e69b226403c885441feb125806b
Vikash Kumar <sharma.vikashkr@gmail.com>
2023-01-01 21:19:48 +0530

    fix

--- src/modules/group/services/group.service.ts
+++ src/modules/group/services/group.service.ts
@@ -134,7 +134,7 @@ export class GroupService {
     }
     const isOwner = extGroup.owner === loggedInUser;
     const xrefResp = await this.userGroupXrefModel.findOne({ groupId: groupId, userId: user._id });
-    const isAdmin = xrefResp.isAdmin;
+    const isAdmin = xrefResp?.isAdmin;
     if (!isOwner && !isAdmin) {
       throw new HttpException("You don't have admin access to this Group", 400);
     }

7dfe1aa0165ff39c5f48b453da8e4221e4eb2d87
Vikash Kumar <sharma.vikashkr@gmail.com>
2023-05-11 22:23:35 +0530

    house GroupView

--- src/modules/house/controllers/house-group.controller.ts
+++ src/modules/house/controllers/house-group.controller.ts
@@ -1,19 +1,15 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { Auth } from 'src/decorators/auth.decorator';
-import { Group } from 'src/entities/group.entity';
 import { HouseGroupService } from '../services/house-group.service';
+import { GroupView } from 'src/entities/vw_group.entity';
 
 @Controller('/house/groups')
 export class HouseGroupController {
-  constructor(
-    private readonly houseGroupService: HouseGroupService,
-  ) {}
+  constructor(private readonly houseGroupService: HouseGroupService) {}
 
   @Auth('USER')
   @Get('/:houseId')
-  getHouseGroups(
-    @Param('houseId') houseId: string,
-  ): Promise<Group[]> {
+  getHouseGroups(@Param('houseId') houseId: string): Promise<GroupView[]> {
     return this.houseGroupService.getHouseGroups(houseId);
   }
 }
